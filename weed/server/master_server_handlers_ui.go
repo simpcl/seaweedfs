955cd32c54f3fbc2f4a40dfa88796566a4161304
Chris Lu <chris.lu@gmail.com>
2017-07-03 22:35:28 -0700

    fix nil raft server caused crash
    fix https://github.com/chrislusf/seaweedfs/issues/524

--- weed/server/master_server_handlers_ui.go
+++ weed/server/master_server_handlers_ui.go
@@ -6,23 +6,23 @@ import (
 	ui "weed/server/master_ui"
 	"weed/stats"
 	"weed/util"
+
+	"github.com/chrislusf/raft"
 )
 
 func (ms *MasterServer) uiStatusHandler(w http.ResponseWriter, r *http.Request) {
 	infos := make(map[string]interface{})
 	infos["Version"] = util.VERSION
 	args := struct {
-		Version  string
-		Topology interface{}
-		Leader   string
-		Peers    interface{}
-		Stats    map[string]interface{}
-		Counters *stats.ServerStats
+		Version    string
+		Topology   interface{}
+		RaftServer raft.Server
+		Stats      map[string]interface{}
+		Counters   *stats.ServerStats
 	}{
 		util.VERSION,
 		ms.Topo.ToMap(),
-		ms.Topo.RaftServer.Leader(),
-		ms.Topo.RaftServer.Peers(),
+		ms.Topo.RaftServer,
 		infos,
 		serverStats,
 	}
