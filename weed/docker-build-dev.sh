3e00c3373677a211152fcf27dc7d58138e85a638
Jiaqi Shen <18863662628@163.com>
2022-07-25 16:48:26 +0800

    add docker build shell

---
+++ weed/docker-build-dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+time=$(date "+%Y-%m-%d")
+current_git_branch_latest_short_id=`git rev-parse --short HEAD`
+sudo docker build --network=host -t harbor-registry.inner.youdao.com/infra/minio/seaweedfs:dev-$time-$current_git_branch_latest_short_id .
+sudo docker push harbor-registry.inner.youdao.com/infra/minio/seaweedfs:dev-$time-$current_git_branch_latest_short_id
